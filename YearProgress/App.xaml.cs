fe98cbb01f466ab64327dcf5159b8947d6d5c153
Colin Kiama <colinkiama@hotmail.co.uk>
2018-04-13 14:18:15 +0100

    App now launches correctly after tile is clicked

--- YearProgress/App.xaml.cs
+++ YearProgress/App.xaml.cs
@@ -115,6 +115,42 @@ namespace YearProgress
             }
         }
 
+        protected async override void OnActivated(IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+            await appStartup();
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+              
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), args);
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
+            }
+
+        
+
         private void RegisterBackgroundTask()
         {
             new NotificationHelper().RegisterBackgroundTasks();

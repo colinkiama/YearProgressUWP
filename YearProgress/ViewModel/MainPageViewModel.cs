28469fd40857cbce2a4a99034be3cb0fb1bb639c
Colin Kiama <colinkiama@hotmail.co.uk>
2018-06-02 18:31:05 +0100

    Sharing Year Progress other apps is now supported!

--- YearProgress/ViewModel/MainPageViewModel.cs
+++ YearProgress/ViewModel/MainPageViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.System;
 using Windows.UI.Xaml;
 using YearProgress.Model;
@@ -19,6 +20,7 @@ namespace YearProgress.ViewModel
 
         public delegate void ClickHandler(object sender, RoutedEventArgs e);
         public ClickHandler myClickHandler;
+        public ClickHandler shareButtonHandler;
 
        public Uri twitterUri = new Uri("https://www.twitter.com/colinkiama");
         public Uri gitHubUri = new Uri("https://www.github.com/colinkiama");
@@ -41,9 +43,28 @@ namespace YearProgress.ViewModel
 
         public MainPageViewModel()
         {
+            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
+            dataTransferManager.DataRequested += DataTransferManager_DataRequested;
+
             DateCalcObject = new DateCalc();
             ViewModelLoaded?.Invoke(this, EventArgs.Empty);
             myClickHandler = new ClickHandler(FeedbackButton_Click);
+            shareButtonHandler = new ClickHandler(ShareButton_Click);
+        }
+
+        private void ShareButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            DataTransferManager.ShowShareUI();
+        }
+
+        private void DataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            DataRequest request = args.Request;
+            request.Data.SetText($"{DateCalcObject.currentDate.Year} is {YearProgress}% complete! - Shared Via Year Progress: https://bit.ly/2JcQEfE");
+            request.Data.Properties.Title = "Year Progress";
+            request.Data.Properties.Description = "Effortlessly share the progress of the year to other apps";
+            
         }
 
         private async void FeedbackButton_Click(object sender, RoutedEventArgs e)

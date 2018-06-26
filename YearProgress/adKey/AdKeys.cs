30d9586a789c555fed6c53ebe6e24b6f0a4c747e
Colin Kiama <colinkiama@hotmail.co.uk>
2018-06-26 22:00:29 +0100

    oo

---
+++ YearProgress/adKey/AdKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YearProgress.adKeys
+{
+    public static class AdKeys
+    {
+        internal const string appId = "9pdq5mljfvsx";
+        internal const string addUnitId = "1100022477";
+    }
+}
